50bab73025251e8c4991ce2a32f52fbbd0737081
Студент <@WSR>
2022-12-27 13:57:06 +0300

    Проверка локального репозитория 2.0!

--- ExamConsole/Program.cs
+++ ExamConsole/Program.cs
@@ -10,7 +10,7 @@ namespace ExamConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("ТестТестТест!");
         }
     }
 }
